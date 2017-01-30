4dda4f733ae9663ff56fb7f40ee31f419ff5ec39
Zach Dyer <zach.dyer@zachdyerdesign.com>
2017-01-30 09:22:42 -0600

    remote ftp

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,12 @@
     <meta charset="utf-8">
     <title>PHP Server</title>
     <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.css">
+    <link rel="stylesheet" href="css/theme.css">
+    <script type="text/javascript" src="js/jquery.min.js"></script>
     <script type="text/javascript" src="js/bootstrap.js"></script>
+
   </head>
   <body>
     <div class="container">
+      
