b72100dcf2163afbce6a84848c73c137e8e4a7e0
Zach Dyer <zach.dyer@zachdyerdesign.com>
2017-02-17 22:53:08 -0600

    nav

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
 <?php include "includes/header.php"; ?>
+<?php include "includes/nav.php"; ?>
 
-<h1>PHP Server</h1>
-<p>Running PHP version 5.5 PHP 7 is not available on our Godaddy server. This server will run on zachdyer.com and use a mysql database. Download the project to the xampp/htdocs/ folder.</p>
+<div class="jumbotron">
+  <h1>PHP Server</h1>
+  <p>Running PHP version <?php echo phpversion() ?> PHP 7 is not available on our Godaddy server. This server will run on zachdyer.com/phpserver and use a mysql database. </p>
+
+  <div class="table-responsive">
+    <table class="table">
+      <tr>
+        <td>PHP Version</td><td><? echo phpversion() ?></td>
+      </tr>
+    </table>
+  </div>
+</div>
+
 
 <?php include "includes/footer.php"; ?>
