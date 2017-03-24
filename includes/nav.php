89fef90de6d8e575d791f4dcea134c59161d8569
Zach Dyer <zach.dyer@zachdyerdesign.com>
2017-03-24 02:09:18 +0000

    fixed logo not showing

--- includes/nav.php
+++ includes/nav.php
@@ -7,7 +7,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="/phpserver/"><span class="glyphicon glyphicon-home"></span> <? echo $config['title'] ?></a>
+      <a class="navbar-brand" href="/phpserver/"><span class="glyphicon glyphicon-home"></span> <?php echo $config['title'] ?></a>
     </div>
     <div id="navbar" class="navbar-collapse collapse">
       <ul class="nav navbar-nav">
